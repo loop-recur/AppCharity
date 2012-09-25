1cf1863d708e1b22bc4681f21b84bde74ae2028d
Will <iamwillking@gmail.com>
2012-09-25 16:07:03 -0700

    events page styling

--- Resources/views/event_row.js
+++ Resources/views/event_row.js
@@ -2,39 +2,57 @@ Views.EventRow = function(event) {
   var self = {
     row: UI.createTableViewRow({
       start_time: new Date(event.start_time),
+      layout: 'horizontal',
       event: event
     }),
 
     cal_icon: UI.createImageView({
       image: '/images/icons/events_calendar.png',
-      height: 41,
-      width: 41,
+      height: 60,
+      width: 50,
+      left: 10,
+      top: 15,
       square: true
     }),
 
-    month: UI.createLabel(merge(Style.h2, {
-      text: 'Oct'
+    month: UI.createLabel(merge(Style.h3, {
+      text: 'Oct',
+      top: 2,
+      height: Ti.UI.SIZE,
+      font: {
+        fontFamily: 'Helvetica Neue',
+        fontSize: 6,
+        fontWeight: 'bold'
+      }
     })),
 
     day: UI.createLabel({
       text: '31',
+      top: 13,
+      height: Ti.UI.SIZE,
       font: {
         fontFamily: 'Helvetica Neue',
-        fontSize: 32,
+        fontSize: 30,
         fontWeight: 'bold'
       }
     }),
 
     content_view: UI.createView({
       layout: 'vertical',
-      left: 80,
-      height: Ti.UI.SIZE
+      left: 10,
+      right: 10,
+      top: 15,
+      bottom: 10,
+      height: Ti.UI.SIZE,
+      width: 235
     }),
 
-    title: UI.createLabel(merge(Style.h1, {
+    title: UI.createLabel(merge(Style.h3, {
       text: event.name,
       color: 'blue',
-      height: Ti.UI.SIZE
+      left: 0,
+      height: Ti.UI.SIZE,
+      width: Ti.UI.SIZE
     })),
 
     time: UI.createLabel(merge(Style.p3, {
@@ -43,13 +61,13 @@ Views.EventRow = function(event) {
       height: Ti.UI.SIZE
     })),
 
-    location: UI.createLabel(merge(Style.p2, {
+    location: UI.createLabel(merge(Style.p3, {
       text: event.location,
       width: Ti.UI.SIZE,
       height: Ti.UI.SIZE
     })),
 
-    description: UI.createLabel(merge(Style.p2, {
+    description: UI.createLabel(merge(Style.p3, {
       text: event.description,
       width: Ti.UI.SIZE,
       height: Ti.UI.SIZE
@@ -57,14 +75,15 @@ Views.EventRow = function(event) {
   };
 
   self.content_view.add(self.title);
-  self.content_view.add(self.time);
-  self.content_view.add(self.location);
+  // self.content_view.add(self.time);
+  // self.content_view.add(self.location);
   self.content_view.add(self.description);
 
   self.cal_icon.add(self.month);
   self.cal_icon.add(self.day);
 
   self.row.add(self.cal_icon);
+  self.row.add(self.content_view);
 
   return self;
 };
--- Resources/windows/events.js
+++ Resources/windows/events.js
@@ -1,16 +1,45 @@
 Windows.Events = function() {
   var self = {
     win: UI.createWindow({
-      layout: 'vertical',
-      navBarHidden: true
+      navBarHidden: true,
+      backgroundImage: '/images/backgrounds/main_bg.png',
+      backgroundRepeat: true
     }),
 
     donate_banner: Views.TopBanner().view,
 
-    table: UI.createTableView()
+    top_shadow: UI.createView({
+      top: 100,
+      width: Ti.UI.FILL,
+      height: 20,
+      backgroundImage: '/images/shadow/screen_shadow_top.png'
+    }),
+
+    side_shadow: UI.createView({
+      top: 120,
+      bottom: 20,
+      backgroundImage: '/images/shadow/screen_shadow_sides_repeat.png',
+      width: Ti.UI.FILL,
+      height: Ti.UI.FILL
+    }),
+
+    bottom_shadow: UI.createView({
+      bottom: 0,
+      width: Ti.UI.FILL,
+      height: 20,
+      backgroundImage: '/images/shadow/screen_shadow_bottom.png'
+    }),
+
+    table: UI.createTableView({
+      top: 100,
+      backgroundColor: 'transparent'
+    })
   };
 
   self.win.add(self.donate_banner);
+  self.win.add(self.top_shadow);
+  self.win.add(self.side_shadow);
+  self.win.add(self.bottom_shadow);
   self.win.add(self.table);
 
   Controllers.Events(self);
