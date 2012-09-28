ef686fa829df963fd8caa3db0933989febd4c5c2
Brian <brian@looprecur.com>
2012-09-28 14:27:45 -0700

    photos further

--- Resources/windows/photo_gallery.js
+++ Resources/windows/photo_gallery.js
@@ -2,15 +2,15 @@ Windows.PhotoGallery = function(){
   
   var self = {
     win: UI.createWindow(),
-    photo_upload_btn: UI.createButton({title: "Upload a new photo", left: 0}), 
-    photo_picker_btn: UI.createButton({title: "Upload a photo from your phone", right: 0}), 
+    view: UI.createView({
+      layout: 'horizontal'
+    }),
+    photo_upload_btn: UI.createButton({title: "Upload a new photo", left: 0})
   };
 
-  self.win.add(self.photo_upload_btn);
-  self.win.add(self.photo_picker_btn);
+  self.win.add(self.view);
 
   Controllers.PhotoGallery(self);
   
   return self;
 }
-
